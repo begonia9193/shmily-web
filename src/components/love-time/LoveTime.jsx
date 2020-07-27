72ea47ca491b0aece91bffad12e86eb977dd1101
Jieyuhua9193 <1500690094@qq.com>
2020-07-27 09:06:53 +0800

    update

--- src/components/love-time/LoveTime.jsx
+++ src/components/love-time/LoveTime.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import moment from 'moment'
+import Style from './index.less'
 
 const getTime = () => {
   let start = moment('2018-07-25 17:20:14')
@@ -8,19 +9,28 @@ const getTime = () => {
     start = start.add(result, dateStr)
     return result
   }
-  const year = timeCalc('year')
-  const month = timeCalc('months')
   const day = timeCalc('days')
   const hour = timeCalc('hours')
   const minutes = timeCalc('minutes')
   const seconds = timeCalc('seconds')
-  return `${year}年${month}个月${day}天 ${hour}小时${minutes}分钟${seconds}秒`
+  return {
+    day,
+    hour,
+    minutes,
+    seconds,
+  }
 }
 
 export default () => {
-  const [time, setTime] = useState(getTime())
+  const [times, setTimes] = useState(getTime())
   setInterval(() => {
-    setTime(getTime())
-  }, 1000);
-  return <h1>{time}</h1>
+    setTimes(getTime())
+  }, 1000)
+  return (
+    <p className={Style.wrap}>
+      第<strong>{times.day}</strong>天<strong>{times.hour}</strong>小时
+      <strong>{times.minutes}</strong>分钟
+      <strong>{times.seconds}</strong>秒
+    </p>
+  )
 }
